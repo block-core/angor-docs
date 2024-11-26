e435d31ff07593aba9386607a4cf5810d6a4f379
Maryam <153804776+maryam-d@users.noreply.github.com>
2024-11-27 00:49:15 +0400

    Update Home page (#2)
    * Update title and tagline
    
    * Updated favicon.svg
    
    * Update home page UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,23 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'Angor Docs',
-      customCss: [
-        './src/styles/style.css',
-      ],
+      title: 'Angor Documentation',
+      customCss: ['./src/styles/custom.scss'],
+      logo: {
+        dark: './src/assets/logo.svg',
+        light: './src/assets/logo.svg',
+        replacesTitle: true,
+      },
       social: {
         twitter: 'https://twitter.com/blockcoredev',
         discord: 'https://www.blockcore.net/discord',
         github: 'https://github.com/block-core/angor',
       },
+      components: {
+        Header: './src/components/overrides/Header.astro',
+        Footer: 'src/components/overrides/Footer.astro',
+        ThemeSelect: 'src/components/overrides/ThemeSelect.astro',
+       },
       sidebar: [
         {
           label: 'Overview',
